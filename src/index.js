e23eba6ddbd84dea29b19fcc499051ea2292453e
chrisgarrity <chrisg@media.mit.edu>
2017-10-17 11:59:48 -0400

    Add scratch-paint
    Breaking change - bumped major version to 2.
    
    Restructured to support multiple components. Package default is just localeData for currently supported locales including the name for each language.
    
    messages for each component are exported as separate files in locales. Clients of l10n will need to import messages for each of the compents used and combine them.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ar from 'react-intl/locale-data/ar';
 import de from 'react-intl/locale-data/de';
 import es from 'react-intl/locale-data/es';
 import he from 'react-intl/locale-data/he';
+import locales from './supported-locales.js';
 
-import {locales} from '../locales/gui-msgs.js';
-
-locales.en.localeData = en;
-locales.ar.localeData = ar;
-locales.de.localeData = de;
-locales.es.localeData = es;
-locales.he.localeData = he;
+let localeData = locales;
+localeData.en.localeData = en;
+localeData.ar.localeData = ar;
+localeData.de.localeData = de;
+localeData.es.localeData = es;
+localeData.he.localeData = he;
 
 export {
-    locales as default
+    localeData as default
 };
