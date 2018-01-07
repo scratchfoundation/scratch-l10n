b32fba4da33cbe2a9810d6b9b0327901b78aca0d
Linan Tao <linan.avvo@gmail.com>
2018-01-07 19:32:08 +0800

    添加中文

--- src/supported-locales.js
+++ src/supported-locales.js
@@ -8,7 +8,8 @@ const locales = {
     ar: {name: 'الْعَرَبِيَّة'},
     de: {name: 'Deutsch'},
     es: {name: 'Español'},
-    he: {name: 'עִבְרִית'}
+    he: {name: 'עִבְרִית'},
+    zh: {name: '中文'}
 };
 
 export {locales as default};
