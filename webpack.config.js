c713d02ee129206f2d49dac398a008c5ef9b5a0a
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-05-07 14:33:47 -0400

    Update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     devtool: 'cheap-module-source-map',
     module: {
         rules: [{
