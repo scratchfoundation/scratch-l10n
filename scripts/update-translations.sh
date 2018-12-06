65b51581495d073648ab5c711c96d0d34d792fe0
chrisgarrity <chrisg@media.mit.edu>
2018-12-06 13:44:47 -0500

    Add clarifying comment

--- scripts/update-translations.sh
+++ scripts/update-translations.sh
@@ -4,9 +4,9 @@
 # exit if either the pulls or the test fail
 
 npm run pull:editor
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi #exit if pull failed
 npm run test
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi #exit if test failed
 git add .
 
 git commit -m "pull new editor translations from Transifex"
