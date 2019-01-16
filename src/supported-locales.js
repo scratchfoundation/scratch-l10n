324f606a194886ea979fce80f8dd83a23676ff71
chrisgarrity <chrisg@media.mit.edu>
2019-01-16 09:45:13 -0500

    Add fully reviewed languages
    Adding Bulgarian, Latvian, Nynorsk, and Slovak.
    
    Removing the obsolete `wwwLocales` export.

--- src/supported-locales.js
+++ src/supported-locales.js
@@ -9,6 +9,7 @@ const locales = {
     'am': {name: 'አማርኛ'},
     'az': {name: 'Azeri'},
     'id': {name: 'Bahasa Indonesia'},
+    'bg': {name: 'български'},
     'ca': {name: 'Català'},
     'cs': {name: 'Česky'},
     'cy': {name: 'Cymraeg'},
@@ -30,7 +31,7 @@ const locales = {
     'zu': {name: 'isiZulu'},
     'is': {name: 'Íslenska'},
     'it': {name: 'Italiano'},
-    // 'lv': {name: 'Latviešu'}, // restore when reviewed
+    'lv': {name: 'Latviešu'},
     'lt': {name: 'Lietuvių'},
     'hu': {name: 'Magyar'},
     'mi': {name: 'Māori'},
@@ -38,7 +39,7 @@ const locales = {
     'ja': {name: '日本語'},
     'ja-Hira': {name: 'にほんご'},
     'nb': {name: 'Norsk Bokmål'},
-    // 'nn': {name: 'Norsk Nynorsk'}, // restore when reviewed
+    'nn': {name: 'Norsk Nynorsk'},
     'th': {name: 'ไทย'},
     'pl': {name: 'Polski'},
     'pt': {name: 'Português'},
@@ -46,7 +47,7 @@ const locales = {
     'ro': {name: 'Română'},
     'ru': {name: 'Русский'},
     'sr': {name: 'Српски'},
-    // 'sk': {name: 'Slovenčina'}, // restore when reviewed
+    'sk': {name: 'Slovenčina'},
     'sl': {name: 'Slovenščina'},
     'fi': {name: 'Suomi'},
     'sv': {name: 'Svenska'},
@@ -99,61 +100,4 @@ const isRtl = locale => {
     return rtlLocales.indexOf(locale) !== -1;
 };
 
-
-// wwwLocales is deprecated. Remove it once we know it's no longer used.
-// For now it's just identical.
-const wwwLocales = {
-    'ab': {name: 'Аҧсшәа'},
-    'ar': {name: 'العربية'},
-    'am': {name: 'አማርኛ'},
-    'az': {name: 'Azeri'},
-    'id': {name: 'Bahasa Indonesia'},
-    'ca': {name: 'Català'},
-    'cs': {name: 'Česky'},
-    'cy': {name: 'Cymraeg'},
-    'da': {name: 'Dansk'},
-    'de': {name: 'Deutsch'},
-    'et': {name: 'Eesti'},
-    'el': {name: 'Ελληνικά'},
-    'en': {name: 'English'},
-    'es': {name: 'Español'},
-    'es-419': {name: 'Español Latinoamericano'},
-    'eu': {name: 'Euskara'},
-    'fr': {name: 'Français'},
-    'ga': {name: 'Gaeilge'},
-    'gd': {name: 'Gàidhlig'},
-    'gl': {name: 'Galego'},
-    'ko': {name: '한국어'},
-    'he': {name: 'עִבְרִית'},
-    'hr': {name: 'Hrvatski'},
-    'zu': {name: 'isiZulu'},
-    'is': {name: 'Íslenska'},
-    'it': {name: 'Italiano'},
-    // 'lv': {name: 'Latviešu'}, // uncomment when reviewed
-    'lt': {name: 'Lietuvių'},
-    'hu': {name: 'Magyar'},
-    'mi': {name: 'Māori'},
-    'nl': {name: 'Nederlands'},
-    'ja': {name: '日本語'},
-    'ja-Hira': {name: 'にほんご'},
-    'nb': {name: 'Norsk Bokmål'},
-    // 'nn': {name: 'Norsk Nynorsk'}, // uncomment when reviewed
-    'th': {name: 'ไทย'},
-    'pl': {name: 'Polski'},
-    'pt': {name: 'Português'},
-    'pt-br': {name: 'Português Brasileiro'},
-    'ro': {name: 'Română'},
-    'ru': {name: 'Русский'},
-    'sr': {name: 'Српски'},
-    // 'sk': {name: 'Slovenčina'}, // uncomment when reviewed
-    'sl': {name: 'Slovenščina'},
-    'fi': {name: 'Suomi'},
-    'sv': {name: 'Svenska'},
-    'vi': {name: 'Tiếng Việt'},
-    'tr': {name: 'Türkçe'},
-    'uk': {name: 'Українська'},
-    'zh-cn': {name: '简体中文'},
-    'zh-tw': {name: '繁體中文'}
-};
-
-export {locales as default, customLocales, localeMap, rtlLocales, isRtl, wwwLocales};
+export {locales as default, customLocales, localeMap, rtlLocales, isRtl};
