10e9f99243f82671a0a90f7721cfa77bcc764a80
Chris Garrity <chrisg@media.mit.edu>
2019-05-09 14:59:05 -0400

    Removed commented out code

--- src/index.js
+++ src/index.js
@@ -54,35 +54,6 @@ import zh from 'react-intl/locale-data/zh';
 import zu from 'react-intl/locale-data/zu';
 
 import locales, {customLocales, localeMap, isRtl} from './supported-locales.js';
-/*
-locales = {
-    'ab': {name: 'Аҧсшәа'},
-    'ca': {name: 'Català'},
-    'cs': {name: 'Česky'},
-    'cy': {name: 'Cymraeg'},
-    'de': {name: 'Deutsch'},
-    'el': {name: 'Ελληνικά'},
-    'en': {name: 'English'},
-    'es': {name: 'Español'},
-    'es-419': {name: 'Español Latinoamericano'},
-    'fr': {name: 'Français'},
-    'ga': {name: 'Gaeilge'},
-    'gd': {name: 'Gàidhlig'},
-    'he': {name: 'עִבְרִית'},
-    'it': {name: 'Italiano'},
-    'ja': {name: '日本語'},
-    'mi': {name: 'Maori'},
-    'nl': {name: 'Nederlands'},
-    'nb': {name: 'Norsk Bokmål'},
-    'pt': {name: 'Português'},
-    'pt-br': {name: 'Português Brasileiro'},
-    'sr': {name: 'Српски'},
-    'sl': {name: 'Slovenščina'},
-    'tr': {name: 'Türkçe'},
-    'uk': {name: 'Українська'},
-    'zh-cn': {name: '简体中文'},
-    'zh-tw': {name: '繁體中文'}
-*/
 
 let localeData = [].concat(
     en,
