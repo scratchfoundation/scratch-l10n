ea17557b55ed490eac2767f8023db1019bcca270
Chris Garrity <chrisg@media.mit.edu>
2019-10-08 07:38:05 -0400

    Correct Swahili name
    correction from Marian M.

--- src/supported-locales.js
+++ src/supported-locales.js
@@ -34,7 +34,7 @@ const locales = {
     'is': {name: 'Íslenska'},
     'it': {name: 'Italiano'},
     'ka': {name: 'ქართული ენა'},
-    'sw': {name: 'kiSwahili'},
+    'sw': {name: 'Kiswahili'},
     'ckb': {name: 'کوردیی ناوەندی'},
     'lv': {name: 'Latviešu'},
     'lt': {name: 'Lietuvių'},
